2213438345a1d1115d10919d29b0d48d16a26303
Jaimin Bariya <jaiminbariyastudent@gmail.com>
2023-12-25 15:02:42 +0530

    practical 2 code done

--- 3. ADBMS/praticlal.sql
+++ 3. ADBMS/praticlal.sql
@@ -234,3 +234,318 @@ FROM (
 
 
 
+-- PRACTICAL ASSIGNMENT-2 ---------------------------------------------------------
+-----------------------------------------------------------------------------------
+
+-- A. Write a procedure to insert data in employee table. ------------------------
+----------------------------------------------------------------------------------
+
+
+CREATE OR REPLACE PROCEDURE InsertEmployee (
+    emp_id_param IN INT,
+    employee_name_param IN VARCHAR2,
+    street_param IN VARCHAR2,
+    city_param IN VARCHAR2
+)
+AS
+BEGIN
+    INSERT INTO employee (emp_id, employee_name, street, city)
+    VALUES (emp_id_param, employee_name_param, street_param, city_param);
+    COMMIT; -- Commit the transaction
+END InsertEmployee;
+
+BEGIN
+InsertEmployee(16, 'JP', 'Surya Nagar', 'Rajkot');
+InsertEmployee(17, 'Arjun', 'Surya Nagar', 'Rajkot');
+END;
+
+select * from employee;
+
+
+
+-- B. Write a procedure to update contact number of employee in employee table.----------
+-----------------------------------------------------------------------------------------
+
+-- first make a column to save contact number
+ALTER TABLE employee
+ADD contact_number VARCHAR2(50);
+
+
+-- make procedure to update contact number
+CREATE OR REPLACE PROCEDURE UpdateContactNumber (
+    emp_id_p IN INT,
+    new_contact_number_p IN VARCHAR2
+)
+AS
+BEGIN
+    UPDATE employee
+    SET contact_number = new_contact_number_p
+    WHERE emp_id = emp_id_p;
+    COMMIT; 
+END UpdateContactNumber;
+
+
+-- update contact number
+BEGIN
+UpdateContactNumber(1, '9090909090');
+UpdateContactNumber(2, '8080808080');
+END;
+
+
+SELECT * FROM employee;
+
+
+
+
+-- C. Write a procedure to find name of manager for given employee id. -----------
+----------------------------------------------------------------------------------
+
+-- make a procedure 
+CREATE OR REPLACE PROCEDURE FindManagerName (
+    emp_id_param IN INT,
+    manager_name OUT VARCHAR2
+)
+AS
+BEGIN
+    SELECT m.manager_name INTO manager_name
+    FROM manager m
+    JOIN manage mg ON m.manager_id = mg.manager_id
+    WHERE mg.emp_id = emp_id_param;
+END FindManagerName;
+
+
+-- find manager name using employee ID
+DECLARE
+    v_manager_name VARCHAR2(100); -- Assuming the manager's name won't exceed 100 characters
+BEGIN
+    FindManagerName(7, v_manager_name);
+    DBMS_OUTPUT.PUT_LINE('Manager Name: ' || v_manager_name);
+END;
+
+
+
+
+-- D. Write a procedure to get all the details (emp_id, name, city of residence, ---------------
+-- company name, city of work, manager name, salary) of given employee id. ---------------------
+
+CREATE OR REPLACE PROCEDURE GetEmployeeDetails (
+    emp_id_param IN INT
+)
+AS
+    v_emp_id employee.emp_id%TYPE;
+    v_employee_name employee.employee_name%TYPE;
+    v_employee_city employee.city%TYPE;
+    v_company_name company.company_name%TYPE;
+    v_company_city company.city%TYPE;
+    v_manager_name manager.manager_name%TYPE;
+    v_salary works.salary%TYPE;
+BEGIN
+    SELECT e.emp_id, e.employee_name, e.city
+    INTO v_emp_id, v_employee_name, v_employee_city
+    FROM employee e
+    WHERE e.emp_id = emp_id_param;
+
+    SELECT c.company_name, c.city
+    INTO v_company_name, v_company_city
+    FROM works w
+    JOIN company c ON w.comp_id = c.comp_id
+    WHERE w.emp_id = emp_id_param;
+
+    SELECT m.manager_name
+    INTO v_manager_name
+    FROM manage mg
+    JOIN manager m ON mg.manager_id = m.manager_id
+    WHERE mg.emp_id = emp_id_param;
+
+    SELECT w.salary
+    INTO v_salary
+    FROM works w
+    WHERE w.emp_id = emp_id_param;
+
+    -- Output the details
+    DBMS_OUTPUT.PUT_LINE('Employee ID: ' || v_emp_id);
+    DBMS_OUTPUT.PUT_LINE('Employee Name: ' || v_employee_name);
+    DBMS_OUTPUT.PUT_LINE('City of Residence: ' || v_employee_city);
+    DBMS_OUTPUT.PUT_LINE('Company Name: ' || v_company_name);
+    DBMS_OUTPUT.PUT_LINE('City of Work: ' || v_company_city);
+    DBMS_OUTPUT.PUT_LINE('Manager Name: ' || v_manager_name);
+    DBMS_OUTPUT.PUT_LINE('Salary: ' || v_salary);
+END GetEmployeeDetails;
+
+
+
+-- to find all detail by emp_id
+BEGIN
+    GetEmployeeDetails(1); -- Provide the employee ID
+END;
+
+
+-- Question 4 ------------------------------------------------------------
+-- Create three tables ---------------------------------------------------
+
+
+CREATE TABLE studentTable (
+roll_no INT PRIMARY KEY,
+name varchar2(50),
+contact varchar2(50),
+marks INT
+);
+
+
+CREATE TABLE studentCopy (
+roll_no INT PRIMARY KEY,
+contact varchar2(50)
+);
+
+
+CREATE TABLE studentUpdateCopy (
+roll_no INT PRIMARY KEY,
+new_contact VARCHAR2(50),
+old_contact varchar2(50)
+);
+
+
+
+CREATE OR REPLACE TRIGGER InsertStudentCopy
+AFTER INSERT ON studentTable
+FOR EACH ROW
+BEGIN
+    INSERT INTO studentCopy (roll_no, contact)
+    VALUES (:NEW.roll_no, :NEW.contact);
+END;
+/
+
+
+-- now add values in studentTable 
+INSERT INTO studentTable VALUES (1, 'Jaimin', '9090909090', '100');
+INSERT INTO studentTable VALUES (2, 'Sagar', '8080808080', '100');
+INSERT INTO studentTable VALUES (3, 'Murtuja', '7878787878', '100');
+INSERT INTO studentTable VALUES (4, 'Rehan', '6060606060', '100');
+
+SELECT * FROM studentTable;
+SELECT * FROM studentCopy;
+
+
+-- B. Create a trigger to insert Roll no New Contact number and old -------
+-- Contact number of student on updation of contact number in Table -------
+-- Student.----------------------------------------------------------------
+
+
+CREATE OR REPLACE TRIGGER UpdateStudentContactCopy
+BEFORE UPDATE OF contact ON studentTable
+FOR EACH ROW
+BEGIN
+    INSERT INTO studentUpdateCopy (roll_no, new_contact, old_contact)
+    VALUES (:OLD.roll_no, :NEW.contact, :OLD.contact);
+END;
+
+
+-- now change contact number 
+UPDATE studentTable
+SET contact = '456'
+WHERE roll_no = 1;
+
+
+-- check in table
+SELECT * FROM studentTable;
+SELECT * FROM studentUpdateCopy;
+
+
+
+-- Question 5: Create a Table ---------------------------------
+---------------------------------------------------------------
+
+CREATE TABLE newStudent (
+roll_no INT PRIMARY KEY,
+name VARCHAR2(50),
+contact VARCHAR2(50),
+marks INT,
+grade varchar2(10)
+
+);
+
+
+INSERT INTO newStudent VALUES (1, 'a', '111', 90, 'A');
+INSERT INTO newStudent VALUES (2, 'b', '222', 10, 'D');
+INSERT INTO newStudent VALUES (3, 'c', '333', 20, 'D');
+INSERT INTO newStudent VALUES (4, 'd', '444', 30, 'D');
+INSERT INTO newStudent VALUES (5, 'e', '555', 40, 'C');
+INSERT INTO newStudent VALUES (6, 'f', '666', 40, 'C');
+INSERT INTO newStudent VALUES (7, 'g', '777', 50, 'C');
+INSERT INTO newStudent VALUES (8, 'h', '888', 60, 'B');
+INSERT INTO newStudent VALUES (9, 'i', '999', 70, 'B');
+INSERT INTO newStudent VALUES (10, j', '000', 80, 'A');
+
+
+-- make a cursor
+DECLARE
+    v_student_count INT := 0; -- Variable to count affected rows
+
+    CURSOR student_cursor IS
+        SELECT *
+        FROM newStudent
+        WHERE marks > 70; -- Select students with marks more than 70
+
+BEGIN
+    -- Open cursor and fetch data
+    FOR student_rec IN student_cursor LOOP
+        -- Display details of students with marks more than 70
+        DBMS_OUTPUT.PUT_LINE('Roll No: ' || student_rec.roll_no || ', Name: ' || student_rec.name || ', Marks: ' || student_rec.marks);
+        
+        -- Increment the affected row count
+        v_student_count := v_student_count + 1;
+    END LOOP;
+
+    -- Display affected row count
+    DBMS_OUTPUT.PUT_LINE('Total affected rows: ' || v_student_count);
+END;
+
+
+
+
+
+-- B. Create an explicit cursor to find 7th record of table Student using loop. -----------
+-------------------------------------------------------------------------------------------
+
+DECLARE
+    -- Declare variables to hold student details
+    v_roll_no newStudent.roll_no%TYPE;
+    v_name newStudent.name%TYPE;
+    v_contact newStudent.contact%TYPE;
+    v_marks newStudent.marks%TYPE;
+    v_grade newStudent.grade%TYPE;
+
+    -- Declare an explicit cursor
+    CURSOR student_cursor IS
+        SELECT *
+        FROM newStudent;
+
+    -- Counter variable for loop
+    v_counter INT := 1;
+
+BEGIN
+    -- Open cursor and fetch data
+    OPEN student_cursor;
+    
+    -- Fetch the 7th record using a loop
+    LOOP
+        FETCH student_cursor INTO v_roll_no, v_name, v_contact, v_marks, v_grade;
+        
+        IF v_counter = 7 THEN
+            -- Display details of the 7th record
+            DBMS_OUTPUT.PUT_LINE('7th Record - Roll No: ' || v_roll_no || ', Name: ' || v_name || ', Contact: ' || v_contact || ', Marks: ' || v_marks || ', Grade: ' || v_grade);
+            EXIT; -- Exit loop after fetching the 7th record
+        END IF;
+        
+        v_counter := v_counter + 1; -- Increment the counter
+    END LOOP;
+
+    -- Close the cursor
+    CLOSE student_cursor;
+END;
+/
+
+
+
+
+
